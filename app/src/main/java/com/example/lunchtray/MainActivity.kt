2b2f8882ceb1e7afee6ea47bb9ced8910d007a0f
DaryugaMS <daryugams@mer.ci.nsu.ru>
2025-10-18 11:47:20 +0700

    lab 3 starter

--- app/src/main/java/com/example/lunchtray/MainActivity.kt
+++ app/src/main/java/com/example/lunchtray/MainActivity.kt
@@ -19,7 +19,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.lunchtray.ui.theme.LunchTrayTheme
-
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
